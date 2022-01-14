0c3137c8c3ca7a636e3b5eb00e32dde488631719
jeremiah-corrado <jcorrado@rams.colostate.edu>
2022-01-14 09:55:13 -0700

    removed fem_domain/target from remote

--- libs/fem_domain/src/domain.rs
+++ libs/fem_domain/src/domain.rs
@@ -8,3 +8,24 @@ pub struct Domain {
     pub mesh: Mesh,
     pub dofs: Vec<DoF>,
 }
+
+impl Domain {
+    pub fn blank() -> Self {
+        Self {
+            mesh: Mesh::blank(),
+            dofs: Vec::new(),
+        }
+    }
+
+    pub fn elems<'a>(&'a self) -> impl Iterator<Item = &'a Elem> + '_ {
+        self.mesh.elems.iter()
+    }
+
+    pub fn edges<'a>(&'a self) -> impl Iterator<Item = &'a Edge> + '_ {
+        self.mesh.edges.iter()
+    }
+
+    pub fn nodes<'a>(&'a self) -> impl Iterator<Item = &'a Node> + '_ {
+        self.mesh.nodes.iter()
+    }
+}
--- libs/fem_domain/target/debug/deps/num_complex-91cc61d5e17a69ad.d
+++
@@ -1,9 +0,0 @@
-/home/jeremiah/Documents/projects/fem_2d_maxwell/libs/fem_2d/libs/fem_domain/target/debug/deps/num_complex-91cc61d5e17a69ad.rmeta: /home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/lib.rs /home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/cast.rs /home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/pow.rs
-
-/home/jeremiah/Documents/projects/fem_2d_maxwell/libs/fem_2d/libs/fem_domain/target/debug/deps/libnum_complex-91cc61d5e17a69ad.rlib: /home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/lib.rs /home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/cast.rs /home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/pow.rs
-
-/home/jeremiah/Documents/projects/fem_2d_maxwell/libs/fem_2d/libs/fem_domain/target/debug/deps/num_complex-91cc61d5e17a69ad.d: /home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/lib.rs /home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/cast.rs /home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/pow.rs
-
-/home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/lib.rs:
-/home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/cast.rs:
-/home/jeremiah/.cargo/registry/src/github.com-1ecc6299db9ec823/num-complex-0.4.0/src/pow.rs:
